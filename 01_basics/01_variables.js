8bab551995b0758486bfab8f30a366860265a3b0
aakinchanycs2408-stack <aakinchany.jain.cs24@ggits.net>
2025-12-01 15:55:34 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = "182238"
+let acoountEmail = "aakinchany@google.com"
+var accountPassword = "357896986"
+accountCity = "jabalpur"
+let accountState;
+
+accountEmail = "jain@google.com"
+accountPassword = "84656985094"
+accountCity = "katni"
+
+
+console.log(accountId);
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountPassword,accountCity,accountEmail,accountState])
